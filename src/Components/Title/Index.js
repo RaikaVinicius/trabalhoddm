1af545cf2b6c4fa54c33927f7b188a6ad9b74c35
RaikaVinicius <raikanobre7@gmail.com>
2024-10-29 11:40:15 -0300

    trabalho DDM

---
+++ src/Components/Title/Index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./Style";
+export default function Title(){
+ return(
+ <View style={styles.Caixa}>
+ <Text style={styles.texTitle}>DDM - Trabalho de DDM - Grupo 2</Text>
+
+ </View>
+ );
+}
+
+
---
+++ src/Components/Title/Style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+ Caixa: {
+ alignItems: "center",
+ },
+ texTitle: {
+ color: "#00CFC1",
+ },
+});
+export default styles
